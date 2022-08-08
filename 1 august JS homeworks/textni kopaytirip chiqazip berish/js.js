f49c8652dced27809fc71a89695a116dba363171
Mukhammadsolikh <tvink16y@gmail.com>
2022-08-08 23:34:14 +0500

    gg

---
+++ 1 august JS homeworks/textni kopaytirip chiqazip berish/js.js
@@ -0,0 +1,22 @@
+
+
+
+var text = prompt("Gap yoki soz yozing")
+var secondtext = prompt("Nechi marta chiqazip berish kerak?") 
+
+function exp(qwe, zxc ) {
+  for(var i = 1; i <= secondtext; i++) {
+    document.write(text +"<br>");  
+}
+}
+
+exp( )
+
+
+
+
+
+
+
+
+
