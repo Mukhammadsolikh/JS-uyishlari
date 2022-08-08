e088009995ec17e9f0a885a83c1b053f3c4f0462
Mukhammadsolikh <tvink16y@gmail.com>
2022-08-08 17:32:16 +0500

    asd

---
+++ 1 august JS homeworks/yulduzcha/app.js
@@ -0,0 +1,30 @@
+
+
+var i = 1
+var y = +prompt('qancha yulduzcha kerak?')//15 
+
+
+var res=""
+
+while(i <= y){  
+    
+   res = res + "*"  
+
+   i++
+   console.log(res)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
